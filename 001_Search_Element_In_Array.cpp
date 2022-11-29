139597bb44fdbbed7699d4cac12be796204dafd0
Raksha Aage <106915505+RakshaAage@users.noreply.github.com>
2022-11-29 23:54:07 +0530

    Create 001_Search_Element_In_Array.cpp

---
+++ 001_Search_Element_In_Array.cpp
@@ -0,0 +1,52 @@
+// Problem Name: Search an Element in an array
+// Problem Link: https://practice.geeksforgeeks.org/problems/search-an-element-in-an-array-1587115621/1?page=1&curated[]=8&sortBy=submissions
+
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    // Function to search x in arr
+    // arr: input array
+    // X: element to be searched for
+    int search(int arr[], int N, int X)
+    {
+    for (int i=0;i<N;i++)
+    {
+    if (X==arr[i]) 
+    {return i;}
+    }
+    return -1;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int testcases;
+    cin>>testcases;
+    while(testcases--)
+    {
+        int sizeOfArray;
+        cin>>sizeOfArray;
+        int arr[sizeOfArray];
+        int x;
+        
+        for(int i=0;i<sizeOfArray;i++)
+        {
+            cin>>arr[i];
+        }
+        cin>>x;
+        Solution ob;
+        cout<<ob.search(arr,sizeOfArray,x)<<endl; //Linear search
+    }
+
+    return 0;
+    
+}
+
+// } Driver Code Ends
