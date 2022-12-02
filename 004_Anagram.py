437a2207ddcbcd0a554a47acf54812fd70711c51
Raksha Aage <106915505+RakshaAage@users.noreply.github.com>
2022-12-02 21:35:46 +0530

    Create 004_Anagram.py

---
+++ 004_Anagram.py
@@ -0,0 +1,27 @@
+//Problem no 4
+//Problem link : https://practice.geeksforgeeks.org/problems/anagram-1587115620/1?page=1&curated[]=8&sortBy=submissions
+
+
+#User function Template for python3
+
+class Solution:
+    #Function is to check whether two strings are anagram of each other or not.
+    def isAnagram(self,a,b):
+        str_a=" ".join(sorted(a))
+        str_b=" ".join(sorted(b))
+        if(str_a==str_b):
+            return True
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__=='__main__':
+    t = int(input())
+    for i in range(t):
+        a,b=map(str,input().strip().split())
+        if(Solution().isAnagram(a,b)):
+            print("YES")
+        else:
+            print("NO") 
+# } Driver Code Ends
